0bc25fd0ac45f41420a72269bd89d3fdc0485750
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-04-29 22:41:57 +0300

    upd: #13 API endpoint products

---
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $products = Product::with('category', 'tags')->get();
+        $products->transform(function ($product) {
+            $product->published_text = $product->is_published ? 'Yes' : 'No';
+            return $product;
+        });
+
+        return $products;
+    }
+}
